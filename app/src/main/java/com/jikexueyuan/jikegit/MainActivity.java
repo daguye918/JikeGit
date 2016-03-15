987d95d6d7cea765eb19cb495017ead772c6dc6a
daguye <daguye918@gmail.com>
2016-03-15 11:51:46 +0800

    修改222

--- app/src/main/java/com/jikexueyuan/jikegit/MainActivity.java
+++ app/src/main/java/com/jikexueyuan/jikegit/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "hello", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "极客学院", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
